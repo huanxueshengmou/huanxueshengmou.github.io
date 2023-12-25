3ebe38eb0080db61b626e65ba0f80b4a337f37c4
huanxue <2759947937@qq.com>
2023-12-25 23:19:01 +0800

    Site updated: 2023-12-25 23:19:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/a40c130c.html","posts/4a027220.html","posts/d30b239a.html","posts/3d0542b6.html","posts/d466e783.html","posts/a361d715.html","posts/db34f9f3.html","posts/534e3aec.html"];function toRandomPost(){
+var posts=["posts/a40c130c.html","posts/d30b239a.html","posts/3d0542b6.html","posts/4a027220.html","posts/d466e783.html","posts/a361d715.html","posts/db34f9f3.html","posts/534e3aec.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
